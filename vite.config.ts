6f3ad7682f02734e2dea623892e7697264c18c36
julio <julio@theowl.solutions>
2023-01-24 00:23:48 -0500

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-app.ts',
-      formats: ['es'],
+      entry: "src/my-app.ts",
+      formats: ["es"],
+      fileName: "index",
     },
     rollupOptions: {
       external: /^lit/,
     },
   },
-})
+});
